2b80b0b55624e9b3d201a6cccf23bf649ae23bda
Adityan Nainar <adityannainar2003@gmail.com>
2023-09-22 12:18:56 +0530

    complete 2 of the tasks in the list

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
+
+console.log("I'm ready!");
+
 // Iteration 1: Names and Input
-//
+
+let hacker1 = "Rameshss"
+console.log(`The driver's name is ${hacker1}`)
+
+let hacker2 = "Suresh"
+console.log(`The navigator's name is ${hacker2}`)
+
 // Iteration 2: Conditionals
 
+if(hacker1.length > hacker2.length){
+    if(hacker1.length === hacker2.length){
+        console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+    }
+    else{
+        console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+    }
+}else{
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+}
 
 // Iteration 3: Loops
